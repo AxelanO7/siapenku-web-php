d7d604bb22dece62f3365fd1943d3fd933314cdc
AxelanO7 <jeremia123.jm@gmail.com>
2024-07-07 20:05:01 +0800

    refactor: Update Kadus and Kasi seeders with new data

--- resources/js/Helper/auth_helper.js
+++ resources/js/Helper/auth_helper.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class ApiHelper {
     baseUrl = "";
     constructor() {
-        this.baseUrl = `http://178.128.16.108/api`;
+        this.baseUrl = `http://159.223.41.12/api`;
     }
     async getBaseUrl() {
         return this.baseUrl;
