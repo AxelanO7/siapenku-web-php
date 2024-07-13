336eaec4c01c104476ac28cbd6ec666069082328
AxelanO7 <axel@btwedutech.com>
2024-07-13 08:24:14 +0800

    refactor: Add LetterSeeder to populate database with sample letter data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             KasiSeeder::class
         ]);
+        $this->call([
+            LetterSeeder::class
+        ]);
     }
 }
---
+++ database/seeders/LetterSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Letter;
+use Illuminate\Database\Seeder;
+
+class LetterSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Letter::create([
+            'name' => 'I Made Sudirsa',
+            'birth_place' => 'Bali',
+            'birth_date' => '1999-01-01',
+            'gender' => 'l',
+            'religion' => 'hindu',
+            'family_card' => '1234567890',
+            'identity_card' => '1234567890',
+            'address' => 'Bulian',
+            'nationality' => 'Indonesia',
+            'needs' => 'KTP',
+            'attachment' => 'logo.png',
+            'status' => 'pending',
+        ]);
+    }
+}
