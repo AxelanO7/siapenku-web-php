abda1c0fe082bbbbfaf92780517222365222696e
AxelanO7 <axel@btwedutech.com>
2024-07-13 18:53:13 +0800

    feat (api): change host url

--- resources/js/Helper/auth_helper.js
+++ resources/js/Helper/auth_helper.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class ApiHelper {
     baseUrl = "";
     constructor() {
-        this.baseUrl = "http://167.99.66.254/api";
+        this.baseUrl = "http://157.245.154.133/api";
         // this.baseUrl = `http://159.223.41.12/api`;
     }
     async getBaseUrl() {
