873bab56b2f0094640331ea19d8cc0609d10930d
AxelanO7 <axel@btwedutech.com>
2024-07-29 14:18:38 +0800

    feat: Add new fields for birth and death information in Letter model and seeder

--- database/migrations/2024_05_15_191435_create_letters_table.php
+++ database/migrations/2024_05_15_191435_create_letters_table.php
@@ -34,6 +34,31 @@ return new class extends Migration
             $table->string('att_family_card')->nullable();
             $table->string('att_certificate')->nullable();
             $table->string('att_rs')->nullable();
+            // birth
+            $table->string('order_child')->nullable();
+            $table->string('birth_attendant')->nullable();
+            $table->string('address_attendant')->nullable();
+            $table->string('identity_card_mother')->nullable();
+            $table->string('name_mother')->nullable();
+            $table->string('birth_place_mother')->nullable();
+            $table->string('birth_date_mother')->nullable();
+            $table->string('address_mother')->nullable();
+            $table->string('identity_card_father')->nullable();
+            $table->string('name_father')->nullable();
+            $table->string('birth_place_father')->nullable();
+            $table->string('birth_date_father')->nullable();
+            $table->string('address_father')->nullable();
+            // death
+            $table->string('name_requester')->nullable();
+            $table->string('identity_card_requester')->nullable();
+            $table->string('address_requester')->nullable();
+            $table->string('relationship_requester')->nullable();
+            $table->string('date_death')->nullable();
+            $table->string('village_death')->nullable();
+            $table->string('sub_district_death')->nullable();
+            $table->string('district_death')->nullable();
+            $table->string('province_death')->nullable();
+            $table->string('cause_death')->nullable();
 
             $table->unsignedBigInteger('kasi_id')->nullable();
             $table->foreign('kasi_id')->references('id')->on('kasis');
