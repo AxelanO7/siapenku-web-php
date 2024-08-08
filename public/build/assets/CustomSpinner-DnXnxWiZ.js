4f6e2ef25e0fa29b2bec6d8485901b1e36859d10
AxelanO7 <axel@btwedutech.com>
2024-08-08 21:43:59 +0800

    refactor: Update API base URL to http://157.230.246.157/api

--- public/build/assets/CustomSpinner-DnXnxWiZ.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{o,e as r,a as s}from"./app-DG7Pnb8T.js";const t={},n={class:"flex justify-center items-center"},c=s("div",{class:"animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"},null,-1),a=[c];function _(i,d){return o(),r("div",n,a)}const m=e(t,[["render",_]]);export{m as C};
