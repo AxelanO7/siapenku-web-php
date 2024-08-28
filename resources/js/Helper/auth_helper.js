6c0a3eeba8464d944f402b32a9737bf2fb892a32
AxelanO7 <axel@btwedutech.com>
2024-08-29 07:41:01 +0800

    refactor: Update API base URL to http://157.230.40.158/api

--- resources/js/Helper/auth_helper.js
+++ resources/js/Helper/auth_helper.js
@@ -4,7 +4,7 @@ class ApiHelper {
     baseUrl = "";
     constructor() {
         // this.baseUrl = `http://localhost:8000/api`;
-        this.baseUrl = `http://68.183.182.78/api`;
+        this.baseUrl = `http://157.230.40.158/api`;
     }
     async getBaseUrl() {
         return this.baseUrl;
