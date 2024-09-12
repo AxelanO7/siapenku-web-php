7a083d5b68f4a58c43884e7a1350280fe1f45250
AxelanO7 <axel@btwedutech.com>
2024-09-12 14:39:44 +0800

    refactor: Update asset file names and imports

---
+++ public/build/assets/CustomSpinner-BhGzUCMJ.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{o,e as r,a as s}from"./app-CCCaBZ_D.js";const t={},n={class:"flex justify-center items-center"},c=s("div",{class:"animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"},null,-1),a=[c];function _(i,d){return o(),r("div",n,a)}const m=e(t,[["render",_]]);export{m as C};
